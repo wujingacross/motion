2353ffa19b572b34723532566f80c0473353d89f
wujing23 <wujing23@58.com>
2023-05-20 11:34:35 +0800

    feat(home): About

--- components/About.js
+++ components/About.js
@@ -1,31 +1,35 @@
-import Image from 'next/image'
-import { motion } from 'framer-motion'
-import { staggerTextContainer } from '../variants'
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { staggerTextContainer, fadeInDown, fadeInUp } from "../variants";
 
 const About = ({ aboutData }) => {
-
-  const { title, subtitle, text, boyImg } = aboutData
-  return <section className='mb-[60px] lg:mb-[160px]'>
-    <div className="container mx-auto">
-      <motion.div
-        variants={staggerTextContainer}
-        initial="initial"
-        whileInView={'animate'}
-        viewport={{ once: false, amount: 0.6 }}
-        className='flex flex-col lg:flex-row gap-x-[20px]'>
-        <div className='flex-1 mt-[74px]'>
-          <h3 className='h3'>{title}</h3>
-          <p className='lead max-w-[740px] mb-[70px]'>{subtitle}</p>
-          <div className='bg-accent/10 border-l-[10px] border-accent max-w-[570px] h-[160px] flex items-center justify-center mb-8 lg:mb-0'>
-            <p className='text-[20px] leading-normal lg:text-[24px] lg:leading-[32px] font-medium max-w-[320px lg:max-w-[460px]'>{text}</p>
-          </div>
-        </div>
-        <div className='flex-1'>
-          <Image src={boyImg} alt="boyImg" width={575} height={480} />
-        </div>
-      </motion.div>
-    </div>
-  </section>;
+  const { title, subtitle, text, boyImg } = aboutData;
+  return (
+    <section className="mb-[60px] lg:mb-[160px]">
+      <div className="container mx-auto">
+        <motion.div
+          variants={staggerTextContainer}
+          initial="initial"
+          whileInView={"animate"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="flex flex-col lg:flex-row gap-x-[20px]"
+        >
+          <motion.div variants={fadeInDown} className="flex-1 mt-[74px]">
+            <h3 className="h3">{title}</h3>
+            <p className="lead max-w-[740px] mb-[70px]">{subtitle}</p>
+            <div className="bg-accent/10 border-l-[10px] border-accent max-w-[570px] h-[160px] flex items-center justify-center mb-8 lg:mb-0">
+              <p className="text-[20px] leading-normal lg:text-[24px] lg:leading-[32px] font-medium max-w-[320px lg:max-w-[460px]">
+                {text}
+              </p>
+            </div>
+          </motion.div>
+          <motion.div variants={fadeInUp} className="flex-1">
+            <Image src={boyImg} alt="boyImg" width={575} height={480} />
+          </motion.div>
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
