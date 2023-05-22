d14a328093da1f4cc660b62dcca5f7d683d50871
wujing23 <wujing23@58.com>
2023-05-22 10:57:20 +0800

    feat: How motion

--- components/How.js
+++ components/How.js
@@ -7,19 +7,25 @@ const How = ({ howData }) => {
   const { title, subtitle, girlImg } = howData
   return <section className='mb-[60px] lg:mb-[160px]'>
     <div className='container mx-auto'>
-      <div className='flex flex-col lg:flex-row gap-x-10 items-center'>
-        <div>
+      <motion.div
+        variants={staggerTextContainer}
+        initial="initial"
+        whileInView={"animate"}
+        viewport={{ once: false, amount: 0.6 }}
+        className='flex flex-col lg:flex-row gap-x-10 items-center'
+      >
+        <motion.div variants={fadeInRight} className="flex-1">
           <Image src={girlImg} alt="girlImg" width={720} height={678} />
-        </div>
-        <div className="flex">
-          <div>
+        </motion.div>
+        <motion.div variants={fadeInLeft} className="flex-1 flex justify-end">
+          <div className="max-w-[445px]">
             <motion.h3 className='h3'>
               {title}
             </motion.h3>
             <p>{subtitle}</p>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   </section>;
 };
