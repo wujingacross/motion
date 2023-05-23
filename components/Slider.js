4222cc9f989a85e7b81714d97cce2cac775d5e64
wujing23 <wujing23@58.com>
2023-05-23 10:46:01 +0800

    feat: Testimonial Slider motion

--- components/Slider.js
+++ components/Slider.js
@@ -1,5 +1,32 @@
-const Slider = () => {
-  return <div className="bg-blue-200">Slider</div>;
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+
+const Slider = ({ clients }) => {
+  return <Swiper
+    slidesPerView={3}
+    spaceBetween={20}
+    slidesOffsetBefore={60}
+    grabCursor={true}
+    className="testimonialSlider"
+  >
+    {clients.map((client, idx) => {
+      const { message, image, name } = client
+      return <SwiperSlide key={idx}>
+        <div className="bg-white h-[360px] drop-shadow-primary rounded-[10px] px-[50px] pt-[60px] pb-[40px] flex flex-col justify-between">
+          <p className="font-light leading-[30px]">{message}</p>
+          <div className="flex items-center gap-x-5">
+            <Image src={image} alt="image" width={60} height={60} />
+            <span className="font-semibold">{name}</span>
+          </div>
+        </div>
+      </SwiperSlide>
+    })}
+
+  </Swiper>;
 };
 
 export default Slider;
+
+
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -5,20 +5,25 @@ import Slider from './Slider'
 
 const Testimonials = ({ testimonialData }) => {
   const { pretitle, title, clients } = testimonialData
-  return <section className="bg-pink-200 relative min-h-[600px]">
+  return <section className="relative min-h-[600px]">
     <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row">
-        <div className="lg:w-[40%]">
+      <motion.div
+        variants={staggerTextContainer}
+        initial="initial"
+        whileInView={'animate'}
+        viewport={{ once: false, amount: 0.6 }}
+        className="flex flex-col lg:flex-row"
+      >
+        <motion.div variants={fadeInUp} className="lg:w-[40%]">
           <h3 className="h3">{pretitle}</h3>
           <h2 className="h2 mb-6">{title}</h2>
-        </div>
-        <div className="lg:w-[60%] lg:absolute lg:right-0">
+        </motion.div>
+        <motion.div variants={fadeInUp} className="lg:w-[60%] lg:absolute lg:right-0">
           <Slider clients={clients} />
-        </div>
-      </div>
-
-    </div>
-  </section>;
+        </motion.div>
+      </motion.div>
+    </div >
+  </section >;
 };
 
 export default Testimonials;
